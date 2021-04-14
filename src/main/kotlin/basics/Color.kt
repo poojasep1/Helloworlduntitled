0aed6ebae254684e762f646547622b930bda78c1
bindu1 <sakalabindu123@gmail.com>
2021-04-14 17:11:37 +0530

    initial commit

---
+++ src/main/kotlin/basics/Color.kt
@@ -0,0 +1,16 @@
+package basics
+
+enum class Color {
+    RED(0xff0000),
+    GREEN(0x00ff00),
+    BLUE(0x0000ff),
+    YELLOW(0xffff00);
+
+    fun containsRed() = (this.rgb and 0xff0000 !=0)
+}
+
+fun main() {
+    val red = Color.RED
+    println(red.containsRed())
+    println(Color.BLUE.containsRed())
+}
